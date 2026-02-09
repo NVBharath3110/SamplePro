42eebc0583958d79223ae3a63a7f2d486a381d7f
Venkata Bharath Nagaswaram <venkata.bharath@nexturn.com>
2026-02-09 10:37:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// Simple JavaScript program
+
+function greet(name) {
+    return `Hello, ${name}! Welcome to Git practice.`;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+// Test outputs
+console.log(greet("Bharath"));
+console.log("Sum:", add(5, 3));
